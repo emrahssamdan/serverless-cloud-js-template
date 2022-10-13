5e42688224492bc6895ffb5d38b1ae11fbb6bb6f
Emrah Samdan <emrahsamdan@gmail.com>
2022-10-13 22:47:29 +0300

    add git

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ import { api } from "@serverless/cloud";
 api.get("/", async (req, res) => {
   res.send("<h1>Hello Serverless Cloud!</h1>");
 });
+
+api.get("/git", async (req, res) => {
+  res.send("<h1>Hello Git!</h1>");
+});
